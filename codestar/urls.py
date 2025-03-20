bff21c7160155d8e7f2ec65eb468511a7af453bd
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-03-20 22:11:15 +0000

    Fix: blog app variable names

--- codestar/urls.py
+++ codestar/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog import views as index_views
+from django.urls import path, include
+from blog import views as blog_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('blog/', blog_views.my_blog, name='blog'),
     path('admin/', admin.site.urls),
 ]
