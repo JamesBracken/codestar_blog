3c738e1cf44bfe5c6fe1c912f265b5cd9858e980
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-03-31 21:49:26 +0100

    Add: new url

--- codestar/settings.py
+++ codestar/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ["codestar-blog-django-test-608a0368997d.herokuapp.com",
                  "herokuapp.com",
                  "127.0.0.1",]
 # ALLOWED_HOSTS = ["*"]
-
+ 
 # Application definition
 
 INSTALLED_APPS = [
