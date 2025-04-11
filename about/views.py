566190bce0b5627bacd83f591c700d0109cf7b2b
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-04-11 20:39:53 +0100

    Add: collaboration request connected to backend

--- about/views.py
+++ about/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import About
 from .forms import CollaborateForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -11,6 +12,17 @@ def about_me(request):
     """
 
     about = About.objects.all().order_by('updated_on').first()
+
+    if request.method == "POST":
+        collaboration_form = CollaborateForm(data=request.POST)
+        if collaboration_form.is_valid():
+            collaboration_form = collaboration_form.save()
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
+
     collaboration_form = CollaborateForm()
 
     return render(
