78c232cb7e456a4b975453fb9bd5217b11ca76f9
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-04-11 10:46:53 +0100

    Add: comments feature

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    template_name = 'blog/index.html'
+    template_name = "blog/index.html"
     paginate_by = 6
 
 
@@ -24,9 +25,15 @@ def post_detail(request, slug):
 
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-    coder = "James"
+    comments = post.comments.all().order_by("-created_on")
+    comments_count = post.comments.filter(approved=True).count()
+
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post,}
+        {
+            "post": post,
+            "comments": comments,
+            "comments_count": comments_count,
+        },
     )
