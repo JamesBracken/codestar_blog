0ce98b83d6929068fe3ecf60622b18b90c80b399
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-04-16 23:17:13 +0100

    Add: successful comment submission test

--- about/test_view.py
+++ about/test_view.py
@@ -13,7 +13,6 @@ class TestAboutViews(TestCase):
         self.about_content.save()
 
     def test_render_about_page(self):
-        # response = self.client.get(reverse("about"))
         response = self.client.get(reverse("about"))
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"GEE WHIZ", response.content)
@@ -21,12 +20,24 @@ class TestAboutViews(TestCase):
             response.context["collaboration_form"], CollaborateForm
         )
 
+    def test_successful_collaboration_request(self):
+        self.client.login(username="myUsername", 
+                          password="myPassword")
+        post_data = {
+            'name': "Johnny",
+            'email': "Johhny@junkmail.com",
+            'message': "Here is some spam for you mate, fyi not the kind you wanted",
+            # 'read': False,
+        }
+        response = self.client.post(reverse(
+            'about'), post_data )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Collaboration request received! I endeavour to respond within 2 working days.", response.content)
 
-
-# The about page loads successfully.
-# The about page contains text from the mock About instance.
-# The collaborate_form context for this view is an instance of CollaborateForm.
-# title
-# updated_on MIGHT NOT HAVE TO
-# content
-# profile_image MIGHT NOT HAVE TO
+    # The assertions 
+    # should check the about page request succeeded and 
+    # that a success message of "Collaboration request received! I endeavour to respond within 2 working days." is included in the response's content.
+    # name = models.CharField(max_length=200)
+    # email = models.EmailField()
+    # message = models.TextField()
+    # read = models.BooleanField(default=False)
